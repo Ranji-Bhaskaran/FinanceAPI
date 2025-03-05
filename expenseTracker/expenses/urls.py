6b5deb1f9b3ba2810a439af73b662c17ce4042af
EC2 Default User <ec2-user@ip-172-31-6-228.eu-west-1.compute.internal>
2025-03-05 20:21:30 +0000

    api lambda la vela seiyum input data va s3 la store pannitu csv aa now tryin to make graph commit

--- expenseTracker/expenses/urls.py
+++ expenseTracker/expenses/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('upload/', views.upload_page, name='upload'),
     path('process-inputs/', views.process_inputs, name='process-inputs'),
-    path('pie_chart/', views.pie_chart_view, name='pie_chart_view'),
     path('api/analysis/<str:file_name>/', views.get_analysis, name='get_analysis')
 ]
