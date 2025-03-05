dba51547ff0b7303e2743e06654ec21c1e6f3b7b
EC2 Default User <ec2-user@ip-172-31-6-228.eu-west-1.compute.internal>
2025-03-05 11:57:38 +0000

    api varaikum work aaguthu aana s3 shud implement to push to dynamo and call it in applicationo commit

--- expenseTracker/expenses/urls.py
+++ expenseTracker/expenses/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     # Pages
     path('', views.home_view, name='home'),
     path('upload/', views.upload_page, name='upload'),
-    path('process-inputs/', views.process_inputs, name='process-inputs')
+    path('process-inputs/', views.process_inputs, name='process-inputs'),
+    path('pie_chart/', views.pie_chart_view, name='pie_chart_view'),
+    path('api/analysis/<str:file_name>/', views.get_analysis, name='get_analysis')
 ]
